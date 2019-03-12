7d7fe0e9de25a0d3f120f9493d2cf8574de45dac
Carter <greasybutton@gmail.com>
2019-03-12 16:52:57 -0700

    basic accordion + anim done

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,23 +1,7 @@
-var acc = document.getElementsByClassName("text-container desktop");
-var i;
+var sections = document.querySelectorAll('input');
 
-for (i = 0; i < acc.length; i++) {
-  // collapse everything but the first entry
-  if (i > 0) {
-    for (var j = 1; j < acc[i].childElementCount; j++) {
-      acc[i].children[j].style.display = "none";
-    }
-  }
-
-  acc[i].firstElementChild.addEventListener("click", function() {
-    /*var children = acc[i].childNodes;
-    for (var j = 0; j < children.length; j++) {
-      var child = children[j];
-      child.style.display = "none";
-    }*/
-
-    // define active
-    // all active
-    // set node to active
-  });
+for (var i = 0; i < sections.length; i++) {
+  var height = sections[i].nextElementSibling.scrollHeight;
+  console.log(height);
+  sections[i].style.height = height;
 }
