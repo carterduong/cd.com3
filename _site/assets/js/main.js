6608b80d9b120099408420e2cbbc69df962d4845
Carter <greasybutton@gmail.com>
2019-03-18 16:22:59 -0700

    refactored scroll function to use scrollIntoView()

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,53 +1,41 @@
 // inital animation
 document.getElementById("info").checked = true;
-/*
-function scrollTo(element, to, duration) {
-  if (duration <= 0) return;
-  var difference = to - element.scrollTop;
-  var perTick = difference / duration * 10;
 
-  setTimeout(function() {
-      element.scrollTop = element.scrollTop + perTick;
-      if (element.scrollTop === to) return;
-      scrollTo(element, to, duration - 10);
-  }, 10);
-}
-*/
-// add eventlistener to each label
-// eventlistener
-// get pixel location of the top of each project
 var inputs = document.querySelectorAll("label");
 var projects = document.querySelectorAll(".project");
 var column = document.getElementsByClassName("projects-container");
 
+window.addEventListener('resize', function() {
+  for (var i = 0; i < inputs.length; i++) {
+    var projectYPos = projects[i].offsetTop;
+    console.log("resize" + i + ": " + projectYPos);
+    inputs[i].addEventListener("click", scroll(document.body, projectYPos, 10), false);
+  }
+});
+
 for (var i = 0; i < inputs.length; i++) {
-  var a = projects[i].offsetTop;
-  inputs[i].addEventListener("click", scroll(document.body, a, 10), false);
+  var projectYPos = projects[i].offsetTop;
+  console.log(projects[i].offsetParent + " " + projectYPos);
+  inputs[i].addEventListener("click", scroll(document.body, projects[i], 10), false);
 }
 
-function scroll(element, to, duration) {
+function scroll(element, destination, duration) {
   return function() {
-    console.log(to);
-
+    console.log(destination);
+    
     column[0].classList.add("fadeIn");
 
-    if (duration <= 0) return;
-    var difference = to - element.scrollTop;
-    var perTick = difference / duration * 10;
-
-    window.scrollTo(0, to);
-    /*
     setTimeout(function() {
-      element.scrollTop = element.scrollTop + perTick;
-      if (element.scrollTop === to) return;
-      scrollTo(element, to, duration - 10);
-    }, 10);
-    */
+      destination.scrollIntoView(true);
+    }, 100);
 
     setTimeout(function() {
       column[0].classList.remove("fadeIn");
-      column[0].classList.remove("fadeIn");
-    }, 3000);
+    }, 2700);
   };
 }
 
+/*
+if bigger that 100vh, go to div's offsetTop
+else center the first image in the project
+*/
