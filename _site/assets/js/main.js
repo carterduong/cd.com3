050fa3667ddb1237eca1623e81e427f772a2b6ab
Carter <greasybutton@gmail.com>
2019-03-19 12:08:00 -0700

    swapped offsetHeight for scrollTo

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,9 +1,13 @@
-// inital animation
+/*** Init ***/
 document.getElementById("info").checked = true;
+
+/*** Variables ***/
 
 var inputs = document.querySelectorAll("label");
 var project_divs = document.querySelectorAll(".project");
 var projects_column = document.getElementsByClassName("projects-container");
+
+/*** Event Listeners ***/
 
 window.addEventListener('resize', function() {
   for (var i = 0; i < inputs.length; i++) {
@@ -13,10 +17,18 @@ window.addEventListener('resize', function() {
   }
 });
 
+window.addEventListener('scroll', function() {
+  console.log(window.scrollY);
+});
+
+// if scrollY > div-distanceToCenter
+
 for (var i = 0; i < inputs.length; i++) {
   var projectYPos = project_divs[i].offsetTop;
   inputs[i].addEventListener("click", scroll(document.body, project_divs[i], 10), false);
 }
+
+/*** Functions ***/
 
 function scroll(element, destination, duration) {
   return function() {
@@ -29,7 +41,7 @@ function scroll(element, destination, duration) {
     } else {
       var distanceToCenter = (window.innerHeight - destination.offsetHeight) / 2;
       setTimeout(function() {
-        element.scrollTop = destination.offsetTop - distanceToCenter;
+        window.scrollTo(0, destination.offsetTop - distanceToCenter);
       }, 100);
     }
 
@@ -38,11 +50,3 @@ function scroll(element, destination, duration) {
     }, 2700);
   };
 }
-
-/*
-if bigger that 100vh
-  go to div's offsetTop
-else 
-  calculate div height
-
-*/
